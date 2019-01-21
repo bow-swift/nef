59d9928dc776955c6f1427f295ef5ef1cb83c7e9
Miguel Ángel Díaz <madiaz.dev@gmail.com>
2019-01-21 15:58:39 +0100

    update configure for bow project

--- setup/TemplateConfigurator.rb
+++ setup/TemplateConfigurator.rb
@@ -10,65 +10,10 @@ module Pod
       @pod_name = pod_name
       @pods_for_podfile = []
       @prefixes = []
-      @message_bank = MessageBank.new(self)
-    end
-
-    def ask(question)
-      answer = ""
-      loop do
-        puts "\n#{question}?"
-
-        @message_bank.show_prompt
-        answer = gets.chomp
-
-        break if answer.length > 0
-
-        print "\nYou need to provide an answer."
-      end
-      answer
-    end
-
-    def ask_with_answers(question, possible_answers)
-
-      print "\n#{question}? ["
-
-      print_info = Proc.new {
-
-        possible_answers_string = possible_answers.each_with_index do |answer, i|
-           _answer = (i == 0) ? answer.underlined : answer
-           print " " + _answer
-           print(" /") if i != possible_answers.length-1
-        end
-        print " ]\n"
-      }
-      print_info.call
-
-      answer = ""
-
-      loop do
-        @message_bank.show_prompt
-        answer = gets.downcase.chomp
-
-        answer = "yes" if answer == "y"
-        answer = "no" if answer == "n"
-
-        # default to first answer
-        if answer == ""
-          answer = possible_answers[0].downcase
-          print answer.yellow
-        end
-
-        break if possible_answers.map { |a| a.downcase }.include? answer
-
-        print "\nPossible answers are ["
-        print_info.call
-      end
-
-      answer
     end
 
     def run
-      Configure.perform(configurator: self)
+      ConfigureBow.perform(configurator: self)
 
       replace_variables_in_files
       clean_template_files
