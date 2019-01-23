61af7726b4497b106570ed269318b13b1c4b0cf9
Miguel Ángel Díaz <madiaz.dev@gmail.com>
2019-01-23 11:21:23 +0100

    clean up playground template

--- template/PROJECT.playground/Pages/bow.xcplaygroundpage/Contents.swift
+++ template/PROJECT.playground/Pages/bow.xcplaygroundpage/Contents.swift
@@ -1,7 +1,1 @@
-/*:
- # Functional Programming in Swift with ![Bow](bow-brand-color.png)
-
- Bow is a Swift library for Typed Functional Programming, available as an open-source project on [Github](https://github.com/bow-swift/bow).
- */
 import Bow
-
