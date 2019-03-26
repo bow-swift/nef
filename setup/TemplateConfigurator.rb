80f2adc02ad7640d97ce9b2a388881fa7545a570
Miguel Ángel Díaz <miguelangel@MacBook-Pro-de-Miguel.local>
2019-03-26 10:34:41 +0100

    remove .gitignore from `nef` project in template generator

--- setup/TemplateConfigurator.rb
+++ setup/TemplateConfigurator.rb
@@ -46,7 +46,7 @@ module Pod
     end
 
     def clean_template_files
-      [".git", ".travis.yml", "LICENSE", "README.md", "bin", "configure", "lib", "markdown", "setup", "template"].each do |asset|
+      [".git", ".gitignore", ".travis.yml", "LICENSE", "README.md", "bin", "configure", "lib", "markdown", "setup", "template"].each do |asset|
         `rm -rf #{asset}`
       end
     end
