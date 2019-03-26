88c090a59148e3697106e6dba827bcb1f0cfc35d
Miguel Ángel Díaz <miguelangel@MacBook-Pro-de-Miguel.local>
2019-03-26 10:39:44 +0100

    minor changes

--- setup/TemplateConfigurator.rb
+++ setup/TemplateConfigurator.rb
@@ -47,7 +47,7 @@ module Pod
     end
 
     def clean_files
-      [".git", ".gitignore", ".travis.yml", "LICENSE", "README.md", "bin", "configure", "lib", "markdown", "setup", "template"].each do |asset|
+      [".git", ".gitignore", ".travis.yml", "LICENSE", "README.md", "bin", "configure", "lib", "markdown", "setup"].each do |asset|
         `rm -rf #{asset}`
       end
     end
