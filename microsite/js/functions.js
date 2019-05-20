2c24fa82f5efdb4840d6b24c89f3a0b76f447d8a
Israel Pérez González <israel.perez.glez@gmail.com>
2019-05-20 15:41:21 +0200

    home

--- microsite/js/functions.js
+++ microsite/js/functions.js
@@ -1,8 +1,17 @@
-function w3_open() {
-    "use strict";
-    document.getElementById("sidebar").style.display = "block";
-}
-function w3_close() {
-    "use strict";
-    document.getElementById("sidebar").style.display = "none";
-}
+$(window).on("load", function () {
+    $(window).scroll(function () {
+        if ($("#site-nav").offset().top > 0) {
+            $("#site-nav").addClass(".nav-scroll");
+        } else {
+            $("#site-nav").removeClass(".nav-scroll");
+        }
+    });
+
+    // Function to load GitHub stats, which expects a DOM element with 'stars' as id
+    (function loadGitHubStats() {
+        var gitHubAPI = "https://api.github.com/repos/bow-swift/bow?callback=?";
+        $.getJSON(gitHubAPI).done(function(data) {
+            $('#stars').text(data.data.stargazers_count);
+        });
+    })();
+});
