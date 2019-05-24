c66b836333aec6d76c9d70bbe391b15f2cba89e9
Juan Valencia Calvellido <juanvalencia@calvellido.es>
2019-05-24 13:47:10 +0200

    Add sidebar open/close features...

--- microsite/js/functions.js
+++ microsite/js/functions.js
@@ -1,6 +1,18 @@
 // This initialization requires that this script is loaded with `defer`
 const navElement = document.querySelector("#site-nav");
 
+/**
+ * Toggle an specific class to the received DOM element.
+ * @param {string}	elemSelector The query selector specifying the target element.
+ * @param {string}	[activeClass='active'] The class to be applied/removed.
+ */
+function toggleClass(elemSelector, activeClass = 'active') {
+  const elem = document.querySelector(elemSelector);
+  if (elem) {
+    elem.classList.toggle(activeClass);
+  }
+}
+
 // Navigation element modification through scrolling
 function scrollFunction() {
   if (document.documentElement.scrollTop > 0) {
@@ -17,4 +29,3 @@ function loadEvent() {
 
 // Attach the functions to each event they are interested in
 window.addEventListener("load", loadEvent);
-
