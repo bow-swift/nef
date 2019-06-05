6064a41b6d03da9d01187ae2e343a25f355e8aa6
Miguel Ángel Díaz <madiaz.dev@gmail.com>
2019-06-05 15:37:02 +0200

    added content for microsite

---
+++ contents/Documentation/Generation.playground/Pages/🔖 Generating a Markdown project.xcplaygroundpage/Contents.swift
@@ -0,0 +1,37 @@
+// nef:begin:header
+/*
+ layout: docs
+ */
+// nef:end
+
+// nef:begin:hidden
+import Bow
+Nef.Playground.needsIndefiniteExecution(false)
+// nef:end
+
+/*:
+ Swift Playgrounds let you write comments in Markdown format using the symbols `//:` for single line comments, or `/`*: ... *`/` for multiline comments. Inside this comments, you can use any Markdown syntax; an exmaple:
+ 
+ ```swift
+ /*:
+ ### This is a heading 1
+ 
+ This is regular text. *This is bold text*. [This is a link](http://bow-swift.io).
+  &#42;/
+ protocol MyProtocol {}
+ 
+ //: ## This is a single line heading 2
+ ```
+ 
+ It makes Swift Playgrounds the proper tool to write an article with compilable examples. The command provided by `nef` to generate the Markdown files is:
+ 
+ ```bash
+ ➜ nef markdown --project <path-to-input> --output <path-to-output>
+ ```
+ 
+ Options:
+ 
+ - `--project`: Path to the folder containing the Xcode project with Swift Playgrounds.
+ - `--output`: Path where the resulting Markdown project will be generated.
+ 
+ */
---
+++ contents/Documentation/Verification.playground/Pages/📃 Creating a Xcode Playground.xcplaygroundpage/Contents.swift
@@ -0,0 +1,70 @@
+// nef:begin:header
+/*
+ layout: docs
+ */
+// nef:end
+
+// nef:begin:hidden
+import Bow
+Nef.Playground.needsIndefiniteExecution(false)
+// nef:end
+
+/*:
+ Xcode Playgrounds are a nice tool for prototyping and trying new concepts. However, third party libraries support is a bit cumbersome to add. One of the goals of `nef` is to make easier the creation of a Xcode Playground with support for one or more libraries.
+ 
+ By default, `nef` can create a Xcode Playground with support for [Bow](http://bow-swift.io), the Functional Programming companion library for Swift.
+ 
+ ```bash
+ ➜ nef playground
+ ```
+ */
+
+/*:
+ ### Command for creating a Xcode Playground
+ You can use the following option to specify the name for the Xcode project that you are creating.
+ 
+ ```bash
+ ➜ nef playground --name LatestBowProject
+ ```
+ 
+ It will create an Xcode project with support for the latest available version of Bow, named `BowPlayground`. If you open this Xcode project, you will have a Xcode Playground where you can import Bow or any of its modules, and start trying some of its features.
+ 
+ &nbsp;
+ 
+ Besides this, you can select any different `Bow` version or branch, even a third-party dependency.
+ 
+ > Note: The next three options are mutually exclusive.
+ 
+ - `--bow-version <x.y.z>`: Specify the version of Bow that you want to use in the project. This option lets you test an old version of the library in a Xcode Playground. Example:
+ 
+ ```bash
+ ➜ nef playground --name OldBowProject --bow-version 0.3.0
+ ```
+ 
+ ##
+ 
+ - `--bow-branch <branch-name>`: Specify the branch of Bow that you want to use in the project. This option lets you test features of Bow that are still in development in a branch that has not been merged or released yet. Example:
+ 
+ ```bash
+ ➜ nef playground --name BranchBowProject --bow-branch master
+ ```
+ 
+ ##
+ 
+ - `--podfile <Podfile>`: Specify a Podfile with your own dependencies. This option lets you create a Playground with support for other libraries. Create a `Podfile` listing your dependencies and pass it to `nef`. Example:
+ 
+ Your `Podfile`, located in `./folder/dependencies`:
+ 
+ ```ruby
+ target 'MyPodsProject' do
+ platform :osx, '10.14'
+ use_frameworks!
+ 
+ pod 'Bow', '~> 0.3.0'
+ end
+ ```
+ 
+ ```bash
+ ➜ nef playground --name MyPodsProject --podfile ./folder/dependencies/Podfile
+ ```
+ */
