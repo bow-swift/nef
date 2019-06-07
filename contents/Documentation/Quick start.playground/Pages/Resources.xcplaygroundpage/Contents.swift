5c9259d76b2d938276eba638cc81c00a459a11ad
Miguel Ángel Díaz <madiaz.dev@gmail.com>
2019-06-07 18:06:15 +0200

    resources section

---
+++ contents/Documentation/Quick start.playground/Pages/Resources.xcplaygroundpage/Contents.swift
@@ -0,0 +1,27 @@
+// nef:begin:header
+/*
+ layout: docs
+ */
+// nef:end
+/*:
+ # Resources
+ 
+ In this section you can find a list of projects, libraries, presentations or blog posts that use Bow:
+ 
+ ## GitHub projects
+ 
+ - [Bow Documentation](https://github.com/bow-swift/bow/tree/master/contents): A microsite which documentation has been built using `nef`. You can see how it looks [here](https://bow-swift.io/docs) and read more about how it works in [this article](https://www.47deg.com/blog/nef-announcement/).
+ 
+ ## Libraries
+ 
+ Be the first one showing here!
+ 
+ ## Presentations
+ 
+ - [Error management and data validation with Bow](https://altconf.miguelangel.me) by Miguel Ángel Díaz-López at AltConf Madrid. June 2019.
+ 
+ ## Blog posts
+ 
+ Be the first one showing here!
+ 
+ */
