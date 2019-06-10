3e1d5efa1c62154efa3fd14a7a77e9599e52c912
Miguel Ángel Díaz <madiaz.dev@gmail.com>
2019-06-10 19:00:13 +0200

    helper for working with URLRequest + String

---
+++ markdown/Carbon/URLQuery.swift
@@ -0,0 +1,25 @@
+//  Copyright © 2019 The nef Authors.
+
+import Foundation
+
+// MARK: Constants
+extension URLRequest {
+    static let URLLenghtAllowed = 3000
+}
+
+// MARK: URL Query <actions>
+extension String {
+    var requestPathEncoding: String {
+        return addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) ?? ""
+    }
+    
+    subscript(truncate: Int) -> String {
+        guard count > truncate else { return self }
+        
+        let sliced = compactMap { $0 }[0..<truncate]
+        let slicedQuery = sliced.dropLast(while: { $0 != "%" }, include: false)
+        return slicedQuery.reduce(into: "") { (acc, char) in acc = "\(acc)\(char)" }
+    }
+}
+
+
