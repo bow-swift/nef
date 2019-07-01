65bd013c6afc1a3145f3e4896c393dfb16ab7111
Miguel Ángel Díaz <madiaz.dev@gmail.com>
2019-07-01 20:14:41 +0200

    update dependencies to carbon fw

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         .target(name: "Common", dependencies: [], path: "markdown/Common"),
         
         .target(name: "NefCarbon", dependencies: ["Markup", "Common"], path: "markdown/NefCarbon"),
-        .target(name: "Nef", dependencies: ["NefCarbon"], path: "markdown/Nef"),
+        .target(name: "Nef", dependencies: ["NefCarbon", "Markup", "Common"], path: "markdown/Nef"),
         
         .target(name: "Markdown", dependencies: ["Markup", "Common"], path: "markdown/Markdown"),
         .target(name: "JekyllMarkdown", dependencies: ["Markup", "Common"], path: "markdown/JekyllMarkdown"),
