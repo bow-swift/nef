181b10bc6f097f904b252a29abba93b7f8acf8c0
Miguel Ángel Díaz <miguelangel.diaz@47deg.com>
2019-07-10 13:46:45 +0200

    output error in carbon api

--- project/Component/NefMarkdown/Markdown.swift
+++ project/Component/NefMarkdown/Markdown.swift
@@ -13,8 +13,8 @@ import Core
 public func renderMarkdown(content: String, to outputPath: String,
                             success: @escaping () -> Void, failure: @escaping (String) -> Void) {
     let outputURL = URL(fileURLWithPath: outputPath)
-    guard let rendered = MarkdownGenerator().render(content: content),
-          let _ = try? rendered.write(to: outputURL, atomically: true, encoding: .utf8) else { failure(""); return }
+    guard let rendered = MarkdownGenerator().render(content: content) else { failure("can not render markdown from input"); return }
+    guard let _ = try? rendered.write(to: outputURL, atomically: true, encoding: .utf8) else { failure("invalid output path"); return }
     
     success()
 }
