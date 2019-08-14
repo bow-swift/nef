38c4f24efe0ac56550fca5453d9c4bfa200a9b29
Miguel Ángel Díaz <miguelangel.diaz@47deg.com>
2019-08-14 20:42:51 +0200

    output for iPad Playgrounds scritpt

---
+++ project/UI/iPad/ConsoleOutput.swift
@@ -0,0 +1,25 @@
+//  Copyright © 2019 The nef Authors.
+
+import Foundation
+import Common
+
+class iPadConsole: ConsoleOutput {
+
+    func printError(information: String) {
+        if !information.isEmpty {
+            print("information: \(information)")
+        }
+        print("error:\(scriptName) could not build Playground compatible with iPad ❌")
+    }
+
+    func printHelp() {
+        print("\(scriptName) --package <package path> --to <output path> --name <playground name>")
+        print("""
+
+                    package: path to Package.swift page. ex. `/home/Package.swift`
+                    to: path where Playground are saved to. ex. `/home`
+                    name: name for the Playground. ex. `Nef`
+
+              """)
+    }
+}
