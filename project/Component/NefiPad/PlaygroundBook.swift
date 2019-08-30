8e09f1a52e1305de663e23f94ebffa523f158f2d
Miguel Ángel Díaz <madiaz.dev@gmail.com>
2019-08-30 16:09:53 +0200

    log for Swift Playground in liveView

--- project/Component/NefiPad/PlaygroundBook.swift
+++ project/Component/NefiPad/PlaygroundBook.swift
@@ -141,6 +141,36 @@ class PlaygroundBook {
                                 static let blue = UIColor(red: 66/255.0, green: 197/255.0, blue: 1, alpha: 1)
                                 static let orange = UIColor(red: 1, green: 159/255.0, blue: 70/255.0, alpha: 1)
                             }
+
+                            enum PlaygroundLog {
+                                static var log: String {
+                                    guard let assessmentStatus = PlaygroundPage.current.assessmentStatus else { return "" }
+
+                                    switch assessmentStatus {
+                                    case let .pass(message): return message ?? ""
+                                    default: return ""
+                                    }
+                                }
+
+                                static func print(_ message: String, clearAfter seconds: Int = 0) {
+                                    let newMessage = \"◦ \\(message)\"
+                                    let assessmentStatus = log.isEmpty ? newMessage : \"\\(log)\\n\\n\\(newMessage)\"
+                                    PlaygroundPage.current.assessmentStatus = .pass(message: assessmentStatus)
+                                    if (seconds > 0) { PlaygroundLog.clear(after: seconds) }
+                                }
+
+                                static func clear(after seconds: Int = 0) {
+                                    guard seconds > 0 else {
+                                        PlaygroundPage.current.assessmentStatus = nil; return
+                                	}
+
+                                    DispatchQueue.main.asyncValueAfter(.now() + .seconds(seconds)) {
+                                        PlaygroundPage.current.assessmentStatus = nil
+                                    }
+                                }
+                            }
+
+                            PlaygroundLog.clear()
                             //#-end-hidden-code
                             liveView.backgroundColor = PlaygroundColor.nef
 
