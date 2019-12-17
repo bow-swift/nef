1d0c8df5f2d948f5c53b716946050ce187b5eac6
Miguel Ángel Díaz <madiaz.dev@gmail.com>
2019-12-17 16:09:44 +0100

    migrated Carbon script to FP

--- project/UI/CarbonPage/main.swift
+++ project/UI/CarbonPage/main.swift
@@ -8,60 +8,81 @@ import NefModels
 import NefCore
 import NefCarbon
 
-let scriptName = "nef-carbon-page"
-let console = CarbonOutput()
+import Bow
+import BowEffects
 
-func main(downloader: CarbonDownloader) {
-    let result = arguments(keys: "from", "to", "background", "theme", "size", "font", "show-lines", "show-watermark")
-    guard let fromPage = result["from"], let output = result["to"] else {
-        ConsoleLegacy.help.show(output: console); exit(-1)
-    }
-    
-    let from = "\(fromPage)/Contents.swift"
-    let playgroundName = PlaygroundUtils.playgroundName(fromPage: from)
-    let to = "\(output)/\(playgroundName)".expandingTildeInPath
-    
-    let backgroundColor = CarbonStyle.Color(hex: result["background"] ?? "") ?? CarbonStyle.Color(default: result["background"] ?? "") ?? CarbonStyle.Color.nef
-    let theme = CarbonStyle.Theme(rawValue: result["theme"] ?? "") ?? CarbonStyle.Theme.dracula
-    let fontType = CarbonStyle.Font(rawValue: result["font"] ?? "") ?? CarbonStyle.Font.firaCode
-    let size = CarbonStyle.Size(factor: result["size"] ?? "") ?? CarbonStyle.Size.x2
-    let lines = result["show-lines"] == "false" ? false : true
-    let watermark = result["show-watermark"] == "false" ? false : true
-    
-    let style = CarbonStyle(background: backgroundColor, theme: theme, size: size, fontType: fontType, lineNumbers: lines, watermark: watermark)
-    renderCarbon(downloader: downloader, from: from, to: to, style: style)
+private let console = Console(script: "nef-carbon-page",
+                              description: "Export Carbon code snippets for given Xcode Playground page",
+                              arguments: .init(name: "page", placeholder: "playground's page", description: "path to playground page. ex. `/home/nef.playground/Pages/Intro.xcplaygroundpage`"),
+                                         .init(name: "output", placeholder: "carbon output", description: "path where Carbon snippets are saved to. ex. `/home`"),
+                                         .init(name: "background", placeholder: "", description: "background color in hexadecimal.", default: "'nef' (#8c44ff)"),
+                                         .init(name: "theme", placeholder: "", description: "carbon's theme.", default: "'dracula'"),
+                                         .init(name: "size", placeholder: "", description: "export file size [1-5].", default: "'2'"),
+                                         .init(name: "font", placeholder: "", description: "carbon's font type.", default: "'firaCode'"),
+                                         .init(name: "show-lines", placeholder: "", description: "shows/hides lines of code [true | false].", default: "'true'"),
+                                         .init(name: "show-watermark", placeholder: "", description: "shows/hides the watermark [true | false].", default: "'true'"),
+                                         .init(name: "verbose", placeholder: "", description: "run carbon page in verbose mode [true | false].", default: "'false'"))
+
+
+func arguments(console: CLIKit.Console) -> IO<CLIKit.Console.Error, (content: String, output: URL, style: CarbonStyle, verbose: Bool)> {
+    console.input().flatMap { args in
+        guard let pagePath = args["page"]?.trimmingEmptyCharacters.expandingTildeInPath,
+              let outputPath = args["output"]?.trimmingEmptyCharacters.expandingTildeInPath else {
+                return IO.raiseError(CLIKit.Console.Error.arguments)
+        }
+        
+        let page = pagePath.contains("Contents.swift") ? pagePath : "\(pagePath)/Contents.swift"
+        let output = URL(fileURLWithPath: "\(outputPath)/\(PlaygroundUtils.playgroundName(fromPage: page))")
+        guard let pageContent = try? String(contentsOfFile: page), !pageContent.isEmpty else {
+            return IO.raiseError(CLIKit.Console.Error.render(information: "could not read page content"))
+        }
+        
+        let backgroundColor = CarbonStyle.Color(hex: args["background"] ?? "") ?? CarbonStyle.Color(default: args["background"] ?? "") ?? CarbonStyle.Color.nef
+        let theme = CarbonStyle.Theme(rawValue: args["theme"] ?? "") ?? CarbonStyle.Theme.dracula
+        let size = CarbonStyle.Size(factor: args["size"] ?? "") ?? CarbonStyle.Size.x2
+        let fontType = CarbonStyle.Font(rawValue: args["font"] ?? "") ?? CarbonStyle.Font.firaCode
+        let lines = args["show-lines"] == "false" ? false : true
+        let watermark = args["show-watermark"] == "false" ? false : true
+        let verbose = args["verbose"] == "true" ? true : false
+        
+        let carbonStyle = CarbonStyle(background: backgroundColor,
+                                      theme: theme,
+                                      size: size,
+                                      fontType: fontType,
+                                      lineNumbers: lines,
+                                      watermark: watermark)
+        
+        return IO.pure((content: pageContent, output: output, style: carbonStyle, verbose: verbose))^
+    }^
 }
 
-/// Renders a page into multiple Carbon images.
-///
-/// - Parameters:
-///   - filePath: input page in Xcode playground format.
-///   - outputPath: output where to render the snippets.
-///   - style: style to apply to export code snippet.
-private func renderCarbon(downloader: CarbonDownloader, from filePath: String, to outputPath: String, style: CarbonStyle) {
-    guard let content = try? String(contentsOf: URL(fileURLWithPath: filePath), encoding: .utf8) else {
-        consoleError(information: ""); return
-    }
+func page(downloader: CarbonDownloader, content: String, output: URL, style: CarbonStyle, verbose: Bool) -> IO<CLIKit.Console.Error, URL> {
+    IO.async { callback in
+        renderCarbon(downloader: downloader,
+                     code: content,
+                     style: style,
+                     outputPath: output.path,
+                     verbose: verbose,
+                     success: { callback(.right(output)) },
+                     failure: { e in callback(.left(.render(information: e))) })
+    }^
+}
+
+@discardableResult
+func main(_ downloader: CarbonDownloader) -> Either<CLIKit.Console.Error, Void> {
+    let args = IOPartial<CLIKit.Console.Error>.var((content: String, output: URL, style: CarbonStyle, verbose: Bool).self)
+    let output = IOPartial<CLIKit.Console.Error>.var(URL.self)
     
-    renderCarbon(downloader: downloader,
-                 code: content,
-                 style: style,
-                 outputPath: outputPath,
-                 success: consoleSuccess,
-                 failure: consoleError)
-}
-
-private func consoleSuccess() {
-    ConsoleLegacy.success.show(output: console)
-    CarbonApplication.terminate()
-}
-
-private func consoleError(information: String) {
-    ConsoleLegacy.error(information: "").show(output: console)
-    CarbonApplication.terminate()
+    return binding(
+           args <- arguments(console: console),
+         output <- page(downloader: downloader, content: args.get.content, output: args.get.output, style: args.get.style, verbose: args.get.verbose),
+    yield: output.get)^
+        .foldM({ e   in console.exit(failure: "\(e)")                                  },
+               { url in console.exit(success: "rendered carbon page in '\(url.path)'") })
+        .unsafeRunSyncEither(on: .global())
 }
 
 // #: - MAIN <launcher - AppKit>
 _ = CarbonApplication { downloader in
-    main(downloader: downloader)
+    main(downloader)
 }
