a8a2c0c69f07b368ef728af9482b759fe054575b
Miguel Ángel Díaz <madiaz.dev@gmail.com>
2019-12-24 14:29:18 +0100

    clean up unuseful parameters for markdown CLI script

--- project/UI/Markdown/main.swift
+++ project/UI/Markdown/main.swift
@@ -8,33 +8,27 @@ import BowEffects
 private let console = Console(script: "nef-markdown",
                               description: "Render markdown files from Xcode Playground",
                               arguments: .init(name: "folder", placeholder: "path-to-input", description: "path to the folder containing Xcode Playgrounds to render"),
-                                         .init(name: "output", placeholder: "path-to-output", description: "path where the resulting Markdown files will be generated"),
-                                         .init(name: "verbose", placeholder: "", description: "run markdown render in verbose mode.", isFlag: true, default: "false"))
+                                         .init(name: "output", placeholder: "path-to-output", description: "path where the resulting Markdown files will be generated"))
 
 
-func arguments(console: CLIKit.Console) -> IO<CLIKit.Console.Error, (folder: URL, output: URL, verbose: Bool)> {
+func arguments(console: CLIKit.Console) -> IO<CLIKit.Console.Error, (folder: URL, output: URL)> {
     console.input().flatMap { args in
         guard let inputPath = args["folder"]?.trimmingEmptyCharacters.expandingTildeInPath,
-              let outputPath = args["output"]?.trimmingEmptyCharacters.expandingTildeInPath,
-              let verbose = Bool(args["verbose"] ?? "") else {
+              let outputPath = args["output"]?.trimmingEmptyCharacters.expandingTildeInPath else {
                 return IO.raiseError(CLIKit.Console.Error.arguments)
         }
         
         let folder = URL(fileURLWithPath: inputPath, isDirectory: true)
         let output = URL(fileURLWithPath: outputPath, isDirectory: true)
 
-        return IO.pure((folder: folder,
-                        output: output,
-                        verbose: verbose))
+        return IO.pure((folder: folder, output: output))
     }^
 }
 
 @discardableResult
 func main() -> Either<CLIKit.Console.Error, Void> {
     arguments(console: console)
-
-//        |<-env.console.printStep(step: step.increment(1), information: "Rendering markdown files for '\(playground.path.filename)'"),
-        .flatMap { (folder, output, verbose) in
+        .flatMap { (folder, output) in
             nef.Markdown.render(playgroundsAt: folder, in: output)
                .provide(console)^
                .mapLeft { _ in .render() }
