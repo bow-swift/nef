e097aaef2837e272e80e3308277383df0d4a6211
Miguel Ángel Díaz <madiaz.dev@gmail.com>
2020-03-12 12:07:43 +0100

    Update project/UI/Nef/main.swift
    Co-Authored-By: Tomás Ruiz-López <truizlop@gmail.com>

--- project/UI/Nef/main.swift
+++ project/UI/Nef/main.swift
@@ -20,7 +20,7 @@ struct NefCommand: ConsoleCommand {
                                                                   CarbonCommand.self])
     
     func main() -> IO<Console.Error, Void> {
-        fatalError("must be invoked a subcommand")
+        fatalError("A subcommand must be invoked.")
     }
 }
 
