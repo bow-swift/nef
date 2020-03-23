ed83d43460e1f31e5fc850745b9f8ed38e58f78d
Miguel Ángel Díaz <madiaz.dev@gmail.com>
2020-03-23 12:44:08 +0100

    api docs for Carbon View

--- project/Component/NefModels/CarbonView.swift
+++ project/Component/NefModels/CarbonView.swift
@@ -2,12 +2,22 @@
 
 import AppKit
 
+/// Describes a Carbon loading view.
 public protocol CarbonLoadingView: NSView {
+    
+    /// Loading view should change its state to show.
     func show()
+    
+    /// Loading view should change its state to hidden.
     func hide()
 }
 
+/// Describes a Carbon view.
 public protocol CarbonView: NSView {
+    
+    /// Associated Carbon loading view.
     var loadingView: CarbonLoadingView? { get set }
+    
+    /// Need to reload the Carbon style.
     func update(state: CarbonStyle)
 }
