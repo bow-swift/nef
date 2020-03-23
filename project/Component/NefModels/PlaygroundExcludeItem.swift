1ee2990471956bbb206d23b836d34df926d7927c
Miguel Ángel Díaz <madiaz.dev@gmail.com>
2020-03-23 12:54:48 +0100

    api docs for PlaygroundExcludeItem

--- project/Component/NefModels/PlaygroundExcludeItem.swift
+++ project/Component/NefModels/PlaygroundExcludeItem.swift
@@ -2,6 +2,10 @@
 
 import Foundation
 
+/// Models a dependency to exclude from `Swift Package`.
+///
+/// - module(name:): the module to exclude from dependencies.
+/// - file(name:module): the filename to exclude from dependencies.
 public enum PlaygroundExcludeItem: Equatable {
     case module(name: String)
     case file(name: String, module: String)
