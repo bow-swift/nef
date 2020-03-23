367e21bcbb79b6fb83d4a71b4be287e36bb8789b
Miguel Ángel Díaz <madiaz.dev@gmail.com>
2020-03-23 13:00:46 +0100

    api docs for PlaygroundDependencies

--- project/Component/NefModels/PlaygroundDependencies.swift
+++ project/Component/NefModels/PlaygroundDependencies.swift
@@ -2,11 +2,22 @@
 
 import Foundation
 
+/// Models the supported `nef Playground` dependencies.
+///
+/// - bow: describes the bow dependencies.
+/// - podfile: describes the CocoaPods dependencies.
+/// - cartfile: describes the Carthage dependencies.
 public enum PlaygroundDependencies {
     case bow(Bow)
     case podfile(URL)
     case cartfile(URL)
     
+    /// Models the `Bow` dependencies.
+    ///
+    /// - version: represents the version number `x.y.z`
+    /// - branch: represents the branch of the repo.
+    /// - tag: represents the tag of the repo.
+    /// - commit: represents the commit hash of the repo.
     public enum Bow {
         case version(String = "")
         case branch(String)
