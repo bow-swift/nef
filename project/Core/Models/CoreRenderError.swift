fcdfdf5aec5ec1e6617a6d2b54d6297370d8449c
Miguel Ángel Díaz <madiaz.dev@gmail.com>
2020-03-25 11:06:35 +0100

    show information about what the render fails

--- project/Core/Models/CoreRenderError.swift
+++ project/Core/Models/CoreRenderError.swift
@@ -2,8 +2,21 @@
 
 import Foundation
 
-public enum CoreRenderError: Error {
+public enum CoreRenderError: Error, Equatable {
     case ast
-    case renderNode
+    case renderNode(String)
     case renderEmpty
+}
+
+extension CoreRenderError: CustomStringConvertible {
+    public var description: String {
+        switch self {
+        case .ast:
+            return "Syntax analysis fails. Review all the delimiters open/close are correct."
+        case .renderNode(let node):
+            return "Could not render the node: \n\(node)\n"
+        case .renderEmpty:
+            return "Render result was empty. Review the page and the nef hidden blocks."
+        }
+    }
 }
