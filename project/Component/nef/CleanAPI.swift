8775e8703da3fdee13d27948962acfcb560300d8
Tomás <truizlop@gmail.com>
2020-03-31 15:05:05 +0200

    Report finalization at the end of command

--- project/Component/nef/CleanAPI.swift
+++ project/Component/nef/CleanAPI.swift
@@ -4,7 +4,6 @@ import Foundation
 import NefCommon
 import NefModels
 import NefClean
-
 import Bow
 import BowEffects
 
@@ -20,5 +19,36 @@ public extension CleanAPI {
                     fileSystem: MacFileSystem(),
                     shell: MacNefPlaygroundSystem()) }
             .mapError { e in nef.Error.compiler(info: "clean: \(e)") }
+        .foldM(
+            { e in
+                EnvIO { progressReport in
+                    progressReport.finished(withError:
+                        CleanCommandOutcome.failed(
+                            nefPlayground.lastPathComponent,
+                            error: e))
+                }
+            },
+            {
+                EnvIO { progressReport in
+                    progressReport.finished(successfully: CleanCommandOutcome.successful(nefPlayground.lastPathComponent))
+                }
+            })
+    }
+}
+
+enum CleanCommandOutcome {
+    case successful(String)
+    case failed(String, error: nef.Error)
+}
+
+extension CleanCommandOutcome: CustomProgressDescription {
+    var progressDescription: String {
+        switch self {
+        case .successful(let name):
+            return "🙌" + " '\(name)' clean up successfully"
+        
+        case let .failed(name, error: error):
+            return "☠️" + " clean up nef Playground '\(name)' failed with error: \(error)"
+        }
     }
 }
