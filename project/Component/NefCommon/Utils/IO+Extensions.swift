93ff7a650543206ced4164a94d29bbf8ec478a35
Tomás <truizlop@gmail.com>
2020-04-01 09:38:20 +0200

    Add function to report finalization of a step

--- project/Component/NefCommon/Utils/IO+Extensions.swift
+++ project/Component/NefCommon/Utils/IO+Extensions.swift
@@ -1,10 +1,21 @@
 import BowEffects
+import NefModels
 
 public extension IO {
     func foldMTap<B>(_ f: @escaping (E) -> IO<E, B>,
                      _ g: @escaping (A) -> IO<E, B>) -> IO<E, A> {
-        handleErrorWith { e in
+        flatTap(g).handleErrorWith { e in
             f(e).followedBy(.raiseError(e))
-        }.flatTap(g)^
+        }^
+    }
+    
+    func step<B: CustomProgressDescription>(
+        _ step: B,
+        reportCompleted progressReport: ProgressReport
+    ) -> IO<E, A> {
+        
+        self.foldMTap(
+            { e in progressReport.failed(step, e) },
+            { _ in progressReport.succeeded(step) })
     }
 }
