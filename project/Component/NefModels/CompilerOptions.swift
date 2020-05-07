2e78198af3e370726cf755e99f063337b9e9c5b3
Miguel Ángel Díaz <madiaz.dev@gmail.com>
2020-05-07 18:41:36 +0200

    complete API reference

--- project/Component/NefModels/CompilerOptions.swift
+++ project/Component/NefModels/CompilerOptions.swift
@@ -2,13 +2,27 @@
 
 import Foundation
 
+/// Swift compiler options
 public struct CompilerOptions {
+    /// List of the resources.
     public let sources: [URL]
+    /// Set SDK for the given platform.
     public let platform: Platform
+    /// Specifies directories to framework search path.
     public let frameworks: [URL]
+    /// Specifies sources which should be passed to the linker.
     public let linkers: [URL]
+    /// Specifies directories to library link search path.
     public let libs: [URL]
     
+    /// Initializes `CompilerOptions`
+    ///
+    /// - Parameters:
+    ///   - sources: list of the resources.
+    ///   - platform: set SDK for the given platform.
+    ///   - frameworks: list of the framworks
+    ///   - linkers: list of the sources which should be passed to the linker.
+    ///   - libs: list of the libraries.
     public init(sources: [URL], platform: Platform, frameworks: [URL], linkers: [URL], libs: [URL]) {
         self.sources = sources
         self.platform = platform
--- project/Component/NefModels/ProgressReport.swift
+++ project/Component/NefModels/ProgressReport.swift
@@ -1,27 +1,43 @@
 import Bow
 import BowEffects
 
+/// Notify about the progress of an event.
 public protocol ProgressReport {
+    /// Notify about the status of an event.
+    /// - Parameter event: progress description.
+    /// - Returns: An `IO` that represent the information for the event.
     func notify<E: Error, A: CustomProgressDescription>(_ event: ProgressEvent<A>) -> IO<E, Void>
 }
 
 public extension ProgressReport {
+    /// Describes a succeeded event.
+    /// - Parameter step: step description.
+    /// - Returns: An `IO` that represent the information for the succeeded event.
     func oneShot<E: Error, A: CustomProgressDescription>(_ step: A) -> IO<E, Void> {
         self.inProgress(step).followedBy(self.succeeded(step))^
     }
     
+    /// Describes an event has just been initiated.
+    /// - Parameter step: step description.
+    /// - Returns: An `IO` that represent the information for the initiated event.
     func inProgress<E: Error, A: CustomProgressDescription>(_ step: A) -> IO<E, Void> {
         self.notify(
             ProgressEvent(step: step,
                           status: .inProgress))
     }
     
+    /// Describes the progress of an event finished with status `succeeded`.
+    /// - Parameter step: step description.
+    /// - Returns: An `IO` that represent the information for the succeeded event.
     func succeeded<E: Error, A: CustomProgressDescription>(_ step: A) -> IO<E, Void> {
         self.notify(
             ProgressEvent(step: step,
                           status: .successful))
     }
     
+    /// Describes the progress of an event finished with status `failed`.
+    /// - Parameter step: step description.
+    /// - Returns: An `IO` that represent the information for the failed event.
     func failed<E: Error, A: CustomProgressDescription>(_ step: A) -> IO<E, Void> {
         self.notify(
             ProgressEvent(step: step,
@@ -29,15 +45,23 @@ public extension ProgressReport {
     }
 }
 
+/// Models the `progress status` for an event.
 public enum ProgressEventStatus {
+    /// Event is running.
     case inProgress
+    /// Event has finished successfully.
     case successful
+    /// Event has failed.
     case failed
 }
 
+/// Models the `step` description for an event.
 public protocol CustomProgressDescription {
+    /// Detailed information about progress in current step.
     var progressDescription: String { get }
+    /// Describes which is the current step for an event.
     var currentStep: UInt { get }
+    /// Describes the number of steps for an event.
     var totalSteps: UInt { get }
 }
 
@@ -46,10 +70,18 @@ public extension CustomProgressDescription {
     var totalSteps: UInt { 1 }
 }
 
+/// Models an `event`.
 public struct ProgressEvent<A: CustomProgressDescription> {
+    /// Describes the current step.
     public let step: A
+    /// Describes the progress status.
     public let status: ProgressEventStatus
     
+    /// Initializes a `Platform`
+    ///
+    /// - Parameters:
+    ///   - step: current step.
+    ///   - status: progress status.
     public init(step: A, status: ProgressEventStatus) {
         self.step = step
         self.status = status
