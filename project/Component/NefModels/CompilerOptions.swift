47f648b68a1fd1319ba46a47a5f071d0a16dac29
Miguel Ángel Díaz <madiaz.dev@gmail.com>
2020-08-03 13:22:37 +0200

    extract compiler dependencies about workspace to its own model

--- project/Component/NefModels/CompilerOptions.swift
+++ project/Component/NefModels/CompilerOptions.swift
@@ -6,28 +6,16 @@ import Foundation
 public struct CompilerOptions {
     /// Source files to compile.
     public let sources: [URL]
-    /// Set SDK for the given platform.
-    public let platform: Platform
-    /// Specifies directories to framework search path.
-    public let frameworks: [URL]
-    /// Specifies sources which should be passed to the linker.
-    public let linkers: [URL]
-    /// Specifies directories to library link search path.
-    public let libs: [URL]
+    /// Workspace options.
+    public let workspace: WorkspaceInfo
     
     /// Initializes `CompilerOptions`
     ///
     /// - Parameters:
     ///   - sources: Source files to compile.
-    ///   - platform: Set SDK for the given platform.
-    ///   - frameworks: List of the frameworks.
-    ///   - linkers: List of the sources which should be passed to the linker.
-    ///   - libs: List of the libraries.
-    public init(sources: [URL], platform: Platform, frameworks: [URL], linkers: [URL], libs: [URL]) {
+    ///   - workspace: Workspace information.
+    public init(sources: [URL], workspace: WorkspaceInfo) {
         self.sources = sources
-        self.platform = platform
-        self.frameworks = frameworks
-        self.linkers = linkers
-        self.libs = libs
+        self.workspace = workspace
     }
 }
